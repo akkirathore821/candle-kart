9ba41127cd0fef4f64b409eac9e35507fcfc5346
Akshay Rathore <akshayrathore@Akshays-MacBook-Air.local>
2025-12-25 12:33:33 +0530

    connection service

--- connection/src/main/java/com/candlekart/connection/controller/ConnectionController.java
+++ connection/src/main/java/com/candlekart/connection/controller/ConnectionController.java
@@ -1,5 +1,7 @@
 package com.candlekart.connection.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/connection")
+@Slf4j
 public class ConnectionController {
     @GetMapping()
-    public ResponseEntity<String> check() {
-        return ResponseEntity.ok("This Connection Service : Connected");
+    public ResponseEntity<String> check(HttpServletRequest request) {
+        log.info("Check Method : Request : " + request.getRemoteAddr());
+        return ResponseEntity.ok("This Connection Service : Connected : " + request.getRemoteAddr());
     }
 }
